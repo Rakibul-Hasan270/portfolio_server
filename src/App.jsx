3b38a51af58f4bb36eb3c6602374f713d6ab3e1a
Nextgent Org <web.nextgent@gmail.com>
2026-09-14 18:20:50 +0600

    modify max weidth

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,8 @@ function App() {
   }, []);
 
   return (
-    <div className='max-w-7xl mx-auto p-4'>
-      
+    <div className='max-w-[1200px] mx-auto p-4'>
+
       <div className='flex justify-center'>
         <div className='h-14 w-full mx-auto rounded-full bottom-0 fixed z-50 max-w-xl backdrop-blur-sm border-2 border-pink-800 flex items-center justify-evenly'>
           <Link className='link text-2xl' to='navbar' smooth={true} spy={true} duration={1000} activeClass='active'>
